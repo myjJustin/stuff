d16fb23c81854023d7280353549c47641aa54c01
myjjustin <78374553+myjJustin@users.noreply.github.com>
2021-06-30 20:41:38 +0200

    initial commit

---
+++ aufgabe 3/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿
+namespace aufgabe_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.cbRot = new System.Windows.Forms.CheckBox();
+            this.cbGelb = new System.Windows.Forms.CheckBox();
+            this.cbGruen = new System.Windows.Forms.CheckBox();
+            this.pRot = new System.Windows.Forms.Panel();
+            this.pGelb = new System.Windows.Forms.Panel();
+            this.pGruen = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.cbLauf = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // cbRot
+            // 
+            this.cbRot.AutoSize = true;
+            this.cbRot.Location = new System.Drawing.Point(79, 39);
+            this.cbRot.Name = "cbRot";
+            this.cbRot.Size = new System.Drawing.Size(43, 17);
+            this.cbRot.TabIndex = 0;
+            this.cbRot.Text = "Rot";
+            this.cbRot.UseVisualStyleBackColor = true;
+            this.cbRot.CheckedChanged += new System.EventHandler(this.cbRot_CheckedChanged);
+            // 
+            // cbGelb
+            // 
+            this.cbGelb.AutoSize = true;
+            this.cbGelb.Location = new System.Drawing.Point(79, 163);
+            this.cbGelb.Name = "cbGelb";
+            this.cbGelb.Size = new System.Drawing.Size(48, 17);
+            this.cbGelb.TabIndex = 1;
+            this.cbGelb.Text = "Gelb";
+            this.cbGelb.UseVisualStyleBackColor = true;
+            this.cbGelb.CheckedChanged += new System.EventHandler(this.cbGelb_CheckedChanged);
+            // 
+            // cbGruen
+            // 
+            this.cbGruen.AutoSize = true;
+            this.cbGruen.Location = new System.Drawing.Point(79, 306);
+            this.cbGruen.Name = "cbGruen";
+            this.cbGruen.Size = new System.Drawing.Size(49, 17);
+            this.cbGruen.TabIndex = 2;
+            this.cbGruen.Text = "Grün";
+            this.cbGruen.UseVisualStyleBackColor = true;
+            this.cbGruen.CheckedChanged += new System.EventHandler(this.cbGruen_CheckedChanged);
+            // 
+            // pRot
+            // 
+            this.pRot.BackColor = System.Drawing.Color.Black;
+            this.pRot.Location = new System.Drawing.Point(233, 39);
+            this.pRot.Name = "pRot";
+            this.pRot.Size = new System.Drawing.Size(106, 100);
+            this.pRot.TabIndex = 3;
+            // 
+            // pGelb
+            // 
+            this.pGelb.BackColor = System.Drawing.Color.Black;
+            this.pGelb.Location = new System.Drawing.Point(233, 163);
+            this.pGelb.Name = "pGelb";
+            this.pGelb.Size = new System.Drawing.Size(106, 100);
+            this.pGelb.TabIndex = 4;
+            // 
+            // pGruen
+            // 
+            this.pGruen.BackColor = System.Drawing.Color.Black;
+            this.pGruen.Location = new System.Drawing.Point(233, 306);
+            this.pGruen.Name = "pGruen";
+            this.pGruen.Size = new System.Drawing.Size(106, 100);
+            this.pGruen.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(467, 163);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 500;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // cbLauf
+            // 
+            this.cbLauf.AutoSize = true;
+            this.cbLauf.Location = new System.Drawing.Point(79, 388);
+            this.cbLauf.Name = "cbLauf";
+            this.cbLauf.Size = new System.Drawing.Size(66, 17);
+            this.cbLauf.TabIndex = 7;
+            this.cbLauf.Text = "Lauflicht";
+            this.cbLauf.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cbLauf);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pGruen);
+            this.Controls.Add(this.pGelb);
+            this.Controls.Add(this.pRot);
+            this.Controls.Add(this.cbGruen);
+            this.Controls.Add(this.cbGelb);
+            this.Controls.Add(this.cbRot);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox cbRot;
+        private System.Windows.Forms.CheckBox cbGelb;
+        private System.Windows.Forms.CheckBox cbGruen;
+        private System.Windows.Forms.Panel pRot;
+        private System.Windows.Forms.Panel pGelb;
+        private System.Windows.Forms.Panel pGruen;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.CheckBox cbLauf;
+    }
+}
+
---
+++ aufgabe 3/Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using Timer = System.Timers.Timer;
+
+namespace aufgabe_3
+{
+    
+    public partial class Form1 : Form
+    {
+        //Dll 
+        [DllImport("iowkit", SetLastError = true)]
+        private static extern int IowKitOpenDevice();
+
+        [DllImport("iowkit", SetLastError = true)]
+        private static extern void IowKitCloseDevice(int iowHandle);
+
+        [DllImport("iowkit", SetLastError = true)]
+        private static extern int IowKitGetNumDevs();
+
+        [DllImport("iowkit", SetLastError = true)]
+        private static extern int IowKitGetDeviceHandle(int numDevice);
+
+        [DllImport("iowkit", SetLastError = true)]
+        private static extern int IowKitWrite(int iowHandle, int numPipe, ref byte buffer, int length);
+        
+        //Timer
+        private Timer aTimer = new Timer();
+
+        private byte[] data = new byte[5];
+        //LEDs
+        private const byte pin17 = 1 << 0;  //grün
+        private const byte pin18 = 1 << 1;  //gleb
+        private const byte pin19 = 1 << 2;  //rot
+
+        int handle;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            data[0]=0;
+            data[1]=0;
+            data[2]=pin17|pin18|pin19;
+            data[3]=0;
+            data[4]=0;
+            update_label();
+            IowKitWrite(handle ,0, ref data[0],5);
+            
+        }
+
+        private void update_label()
+        {
+            label1.Text = "data[2]= " + string.Format("{0:X2}", data[2]) + "(" + data[2].ToString() + ")";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            handle = IowKitOpenDevice();
+
+        }
+        int se;
+        private void cbRot_CheckedChanged(object sender, EventArgs e)
+        {
+            
+            if (cbRot.Checked == true)
+            {
+                pRot.BackColor = Color.Red;
+                se =  0x04;
+                data[2] &= (byte)(~Convert.ToByte(se));
+                IowKitWrite(handle, 0, ref data[0], 3);
+                update_label();
+            }
+            else
+            {
+                data[2] |= Convert.ToByte(se);
+                pRot.BackColor = Color.Black;
+                update_label();
+            }
+        }
+        int ss;
+        private void cbGelb_CheckedChanged(object sender, EventArgs e)
+        {
+            
+            if (cbGelb.Checked == true)
+            {
+                pGelb.BackColor = Color.Yellow;
+                ss =  0x02;
+                data[2] &= (byte)(~Convert.ToByte(ss));
+                IowKitWrite(handle, 0, ref data[0], 3);
+                update_label();
+            }
+            else
+            {
+                data[2] |= Convert.ToByte(ss);
+                pGelb.BackColor = Color.Black;
+                update_label();
+            }
+
+        }
+        int sd;
+        private void cbGruen_CheckedChanged(object sender, EventArgs e)
+        {
+            
+            if (cbGruen.Checked == true)
+            {
+                pGruen.BackColor = Color.Green;
+                sd =  0x01;
+                data[2] &=(byte)(~ Convert.ToByte(sd));
+                IowKitWrite(handle, 0, ref data[0], 3);
+                update_label();
+            }
+            else
+            {
+                data[2] |= Convert.ToByte(sd);
+                pGruen.BackColor = Color.Black;
+                update_label();
+
+            }
+                   
+
+        }
+        int lauflicht;
+        
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            update_panel();
+            IowKitWrite(handle, 0, ref data[0],5);
+            update_label();
+            lauflicht++;
+            if (cbLauf.Checked)
+            {
+                cbLauf_CheckedChanged();
+                update_label();
+                
+            }
+
+        }
+
+        private void cbLauf_CheckedChanged()
+        {
+            int cache = 7;
+
+            update_label();
+            if (lauflicht >= 3)
+            {
+                lauflicht = 0;
+            }
+            switch (lauflicht)
+            {
+                case 0:
+                    if (cbRot.Checked)
+                        cache &= ~pin19;
+                    break;
+                case 1:
+                    if (cbGelb.Checked)
+                        cache &= ~pin18;
+                    break;
+                case 2:
+                    if (cbGruen.Checked)
+                        cache &= ~pin17;
+                    break;
+            }
+            data[2] = (byte)cache;
+        }
+        private void update_panel()
+        {
+            panelxd(pin17).BackColor=refundfarbe(pin17);
+            panelxd(pin18).BackColor = refundfarbe(pin18);
+            panelxd(pin19).BackColor = refundfarbe(pin19);
+        }
+
+        private Color refundfarbe(byte pin)
+        {
+            switch (pin)
+            { case pin17:
+                    return (data[2] & pin) == 0 ? Color.Green : Color.Black;
+               case pin18:
+                    return (data[2] & pin) == 0 ? Color.Yellow : Color.Black;
+                case pin19:
+                    return (data[2] & pin) == 0 ? Color.Red :Color.Black;
+                default:
+                    return Color.Black;
+
+
+
+            }
+        }
+        private Panel panelxd(byte pin)
+        {
+            switch (pin)
+            {
+                case pin17:
+                    return pGruen;
+                case pin18:
+                    return pGelb;
+                case pin19:
+                    return pRot; 
+                default:
+                    return null;
+
+
+
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            IowKitCloseDevice(handle);
+        }
+    }
+}
